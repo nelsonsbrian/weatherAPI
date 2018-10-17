00785f107a03b0853903984a26572c7d63638cca
Brian Nelson and Regina Nurieva <brian@>
2018-10-17 13:37:32 -0700

    add promise logic and add weather-service js link

--- src/main.js
+++ src/main.js
@@ -2,27 +2,25 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { WeatherService } from "./weather-service";
 
 $(document).ready(function() {
-    $('#weatherLocation').click(function() {
-      let city = $('#location').val();
-      $('#location').val("");
-      $.ajax({
-        url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ac228ce27f7a3e6d033a9dc467a754ba`,
-        type: 'GET',
-        data: {
-          format: 'json'
-        },
-        success: function(response) {
-          $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-          $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
-          $('.clouds').text(`The cloudiness is ${response.clouds.all}%.`);
-          $('.description').text(`${response.weather[0].main}: ${response.weather[0].description} with a wind speed of ${response.wind.speed} mph`);
-        },
+  $('#weatherLocation').click(function() {
+    let city = $('#location').val();
+    $('#location').val("");
 
-        error: function() {
-          $('#errors').text("There was an error processing your request. Please try again.");
-        }
-      });
+    let weatherService = new WeatherService();  // create instance of WeatherService class
+    let promise = weatherService.getWeatherByCity(city);  // call the instance method and pass in user input
+
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp}.`);
+      $('.clouds').text(`The cloudiness is ${body.clouds.all}%.`);
+      $('.description').text(`${body.weather[0].main}: ${body.weather[0].description} with a wind speed`);
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
     });
   });
+});
+
